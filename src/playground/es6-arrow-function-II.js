d2e0ef01387d2802bcc252fffdcbe9f6d54d3bdd
Daniel Kreling <dbkreling@gmail.com>
2018-01-31 07:17:59 -0200

    playground: add some ES6 arrow function examples
    Playground is being added with two new files about arrow functions with
    examples of what works and what not, and a challenge regarding map() and
    forEach() functions.
    
    Also some examples of var, let and const types differences are included.
    
    Ref.: Lectures
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/playground/es6-arrow-function-II.js
+++ src/playground/es6-arrow-function-II.js
@@ -1,62 +1,50 @@
-'use strict';
-
 // arguments is no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
   // console.log(arguments);
   return a + b;
-};
+}
 
 console.log(add(10, 1));
 
 //this is no longer bound with objects
 
-var user = {
+const user =  {
   name: 'Daniel',
   cities: ['Batavia', 'New York', 'Dublin'],
   // printCitiesLived: function () {  // it works: ES5 syntax: this bound to user
-  printCitiesLived: function printCitiesLived() {
-    var _this = this;
-
-    // New ES6 syntax that works
+  printCitiesLived() { // New ES6 syntax that works
     // this.cities.forEach(function (city) {  --> fails: 'this' bound to the function scope
-    this.cities.forEach(function (city) {
-      console.log(_this.name + ' has lived in ' + city);
+     this.cities.forEach((city) => {
+      console.log(this.name + ' has lived in ' + city);
+      
     });
   }
-};
+}
 
 user.printCitiesLived();
 
 // The same block using the map() function
-var userTwo = {
+const userTwo =  {
   name: 'Daniel',
   cities: ['Ribeirao', 'Batavia', 'Floripa'],
-  printCitiesLived: function printCitiesLived() {
-    var _this2 = this;
-
-    return this.cities.map(function (city) {
-      return _this2.name + ' has lived in ' + city;
-    });
+  printCitiesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
   }
-};
+}
 
 console.log(userTwo.printCitiesLived());
 
 // challenge:
 
-var multiplier = {
+const multiplier = {
   // numbers = array of numbers
   // multiplyBy = single number
   // multiply = returns an array where the numbers have been multiplied
   numbers: [10, 20, 30],
   multiplyBy: 10,
-  multiply: function multiply() {
-    var _this3 = this;
-
-    return this.numbers.map(function (num) {
-      return num * _this3.multiplyBy;
-    });
+  multiply() {
+    return this.numbers.map((num) => num * this.multiplyBy)
   }
 };
 
