06f20b608e2052906ab7699d0d1882556fae86a0
Daniel Kreling <dbkreling@gmail.com>
2018-02-01 17:29:36 -0200

    app: move counter example to its own file
    The counter example served its purpose to show how counters and rendering
    work on the screen and now has no direct use for us anymore in app.js.
    
    Ref.: Lecture 18
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -43,50 +43,6 @@ var template = React.createElement(
   )
 );
 
-var count = 0;
-var addOne = function addOne() {
-  count++;
-  renderToTheScreen();
-};
-var minusOne = function minusOne() {
-  count--;
-  renderToTheScreen();
-};
-var reset = function reset() {
-  count = 0;
-  renderToTheScreen();
-};
-
 var appRoot = document.getElementById('app');
 
-var renderToTheScreen = function renderToTheScreen() {
-  var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      'Count: ',
-      count
-    ),
-    React.createElement(
-      'button',
-      { onClick: addOne },
-      '+1'
-    ),
-    React.createElement(
-      'button',
-      { onClick: minusOne },
-      '-1'
-    ),
-    React.createElement(
-      'button',
-      { onClick: reset },
-      'reset'
-    )
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderToTheScreen();
+ReactDOM.render(template, appRoot);
