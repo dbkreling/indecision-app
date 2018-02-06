6d4ca89dbacc124d87d2926344570d16077a7379
Daniel Kreling <dbkreling@gmail.com>
2018-02-06 07:07:09 -0200

    app.js: add first forms and buttons
    This commit adds the first form of the application that adds an option
    to an array of values.
    
    It also adds two buttons to add and remmove all items.
    
    Ref.: Lecture 18
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,43 +6,83 @@ console.log('App.js is running');
 var app = {
   title: 'Indecision App',
   subTitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subTitle && React.createElement(
-    'p',
-    null,
-    app.subTitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options:' : 'No Options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item 1'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item 2'
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value; // e.target is form, and access the value of the element named option
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = ''; // clear the option just added
+    render();
+  }
+};
+
+var removeAll = function removeAll() {
+  app.options = [];
+  render();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subTitle && React.createElement(
+      'p',
+      null,
+      app.subTitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options:' : 'No Options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: removeAll },
+      'Remove all'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item 1'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item 2'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -4,21 +4,47 @@ console.log('App.js is running');
  const app = {
   title: 'Indecision App',
   subTitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subTitle && <p>{app.subTitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
-    <ol>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </ol>
-  </div>
-);
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
-const appRoot = document.getElementById('app')
+  const option = e.target.elements.option.value;  // e.target is form, and access the value of the element named option
 
-ReactDOM.render(template, appRoot);
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';  // clear the option just added
+    render()
+  }
+};
+
+const removeAll = () => {
+  app.options = [];
+  render();
+};
+
+const appRoot = document.getElementById('app');
+
+let render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subTitle && <p>{app.subTitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={removeAll}>Remove all</button>
+      <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+
+render()
