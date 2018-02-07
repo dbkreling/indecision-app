ff2447cc0f2dd178072db4ddead682f11a320dfc
Daniel Kreling <dbkreling@gmail.com>
2018-02-07 06:50:58 -0200

    app.js: add makeDecision function
    The onMakeDecision function will be fired when the 'What Should I Do?' button
    is pressed, and it will pick an option among the elements of the 'options' array,
    inside the 'app' object.
    
    With this function the application is a complete functional, however simple,
    application.
    
    Ref.: Lecture 20
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/app.js
+++ src/app.js
@@ -24,6 +24,11 @@ const removeAll = () => {
   render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  alert(app.options[randomNum]);
+};
+
 const appRoot = document.getElementById('app');
 
 let render = () => {
@@ -32,12 +37,10 @@ let render = () => {
       <h1>{app.title}</h1>
       {app.subTitle && <p>{app.subTitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
-      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0}  onClick={onMakeDecision}>What Should I Do?</button>
       <button onClick={removeAll}>Remove all</button>
       <ol>
-        {
-          app.options.map((option) => <li key={option}>{option}</li>)
-        } 
+        {app.options.map((option) => <li key={option}>{option}</li>)} 
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option" />
