d0421e4b1986b774f181832256a2a653c6957b00
Daniel Kreling <dbkreling@gmail.com>
2018-02-08 22:03:18 -0200

    app.js: nest components
    This commit nests all the components called in jsx to a new component
    IndecisionApp. The Options component also nests the AddOption component and
    builds up the structure that will be usend in the app.
    
    Ref.: Lecture 27
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (<div>
+      <Header />
+      <Action />
+      <Options />
+      <AddOption />
+    </div>)
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,7 +35,16 @@ class Options extends React.Component {
     return (
       <div>
         <p>Options component here.</p>
+        <Option />
       </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return(
+      <p>Option component here.</p>
     );
   }
 }
@@ -39,13 +59,4 @@ class AddOption extends React.Component{
   }
 }
 
-const jsx = (
-  <div>
-  <Header />
-  <Action />
-  <Options />
-  <AddOption />
-</div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
