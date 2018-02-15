8b2208382b7d9256b66c8720d09a0be138703cc4
Daniel Kreling <dbkreling@gmail.com>
2018-02-15 07:41:33 -0200

    playground: rebuild VisibilityToggle API using React Components
    Now that React Component and Component State have been used, we can put them
    in practice in the VisibilityToggle API.
    
    This commit rebuilds it using these React features and refactors the text to
    reflect the changes.
    
    Ref.: Lecture 35
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,29 +1,44 @@
-let visible = false;
+class VisibilityToggle extends React.Component {
 
-const toggle = () => {
-  console.log('inside toggle');
-  visible = !visible;
-  render();  
-}
-
-let render = () => {
-  const template = (
-    <div>
-      <h1>Reveal a Secret App</h1>
-      <p>Press the button below to see a secret message</p>
-      <button onClick={toggle}>{visible ? 'Hide secret' : 'Show Secret'}</button>
-      {visible ? 
-        <p>This is a secret message. Press the button again to hide it.</p> &&
-        <ol>
-          <li>Create render function and const with HTML/JS inside and call it to render on the screen</li>
-          <li>Add button and set 'visible' variable</li>
-          <li>Create 'toggle' function</li>
-          <li>Add text to display inside the logic wired with the 'visible' variable</li>
-        </ol> : ''}
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.handleToggleVisible = this.handleToggleVisible.bind(this);
+    this.state = {
+      visible: false
+    };
+  };
   
-  ReactDOM.render(template, document.getElementById('app'));
+  handleToggleVisible() {
+    this.setState((prevState) => {
+      return {
+        visible: !prevState.visible
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Reveal the Steps to this App</h1>
+        <p>Press the button below to see a the steps to build this API</p>
+        <button onClick={this.handleToggleVisible}>
+          {this.state.visible ? 'Hide Steps' : 'Show Steps'}
+        </button>
+        {
+          this.state.visible ?
+          <p>This is a secret message. Press the button again to hide it.</p> &&
+          <ol>
+            <li>Create VisibilityToggle class that extends React.Component</li>
+            <li>Create render function, insert divs with HTML/JS and add ReactDOM line at the bottom. This should be working right now.</li>
+            <li>Add button and set the constructor method. Remember to bind 'this' and to create a state object (visible variable equals false).</li>
+            <li>Create a handleToggleVisible method with the setState inside.</li>
+            <li>Inside the method, invert the prevState.visible variable value.</li>
+            <li>Add text to display inside the logic wired with the 'this.state.visible' variable</li>
+          </ol> : ''
+        }
+      </div>
+    );
+  };
 }
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
