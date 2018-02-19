c6e0c4994574f4441cf8e926f5ad8ddcc396d0f6
Daniel Kreling <dbkreling@gmail.com>
2018-02-19 17:39:50 -0300

    app.js: add methods and reverse data flow
    This commit introduces functions being passed as props to methods, so
    buttons can alter objects of the app, such as 'options'. This way the data
    flow is being reversed, allowing a child to communicate with the parent by
    calling the method.
    
    The methods being added with this commit are 'handleDeleteOptions' that
    is passed to Options and removes all the elements of the 'options' array.
    
    Also, 'handlePick' is being implemented with the logic of the src/playground/
    jsx-indecision-app.js, to pick one of the elements of the array and display it
    as an alert on the browser screen
    
    Ref.: Lecture 36
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,15 +1,43 @@
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.state = {
+      options: ['one', 'two', 'four']
+    };
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => {
+      return {
+        options: []
+      }
+    });
+  }
+
+  handlePick() {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      alert(this.state.options[randomNum]);
+  }
+
   render() {
     const title='Indecision';
     const subtitle='Put your life in the hands of a computer';
-    const options=['one', 'two', 'four'];
 
     return (
       <div>
         <Header title={title} subtitle={subtitle}/>
-        <Action />
-        <Options options={options}/>
-        <AddOption />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption
+        />
       </div>)
   }
 }
@@ -20,39 +48,31 @@ class Header extends React.Component {
       <div>
         <h1>{this.props.title}</h1>
         <h2>{this.props.subtitle}</h2>
-      </div>  
+      </div>
     );
   }
 }
 
 class Action extends React.Component {
-  handlePick() {
-    alert('handlePick');
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What Should I Do?</button>
+        <button
+          disabled={!this.props.hasOptions}
+          onClick={this.props.handlePick}
+        >
+          What Should I Do?
+        </button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-  }
-
-  handleRemoveAll() {
-    alert('handleRemoveAll');
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
         {this.props.options.map((option) => <Option key={option} optionText={option} />)}
       </div>
     );
