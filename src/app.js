5569a5343b90cb3ab9cc17a2977ea927a4a0db38
Daniel Kreling <dbkreling@gmail.com>
2018-02-21 07:24:32 -0300

    app.js: add button to remove a singular option
    When the option text is passed to AddOption in order to be rendered on the
    screen, a new button 'remove' will be passed along with it. This button
    is currently console.logging a string and the event information on the console.
    
    This is a commit to implement the functionality and the final logic will be
    coming up next in this same patchset (branch).
    
    Ref.: Lecture 43
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ class IndecisionApp extends React.Component {
   constructor(props) {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.handlePick = this.handlePick.bind(this);
     this.state = {
@@ -11,6 +12,10 @@ class IndecisionApp extends React.Component {
 
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
+  }
+
+  handleDeleteOption(option) {
+    console.log('HDO', option);
   }
 
   handlePick() {
@@ -43,6 +48,7 @@ class IndecisionApp extends React.Component {
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption
           handleAddOption={this.handleAddOption}
@@ -85,7 +91,13 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
-      {props.options.map((option) => <Option key={option} optionText={option} />)}
+      {props.options.map((option) => (
+        <Option
+          key={option}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}
+        />
+      ))}
     </div>
   );
 }
@@ -94,6 +106,7 @@ const Option = (props) => {
   return(
     <div>
       {props.optionText}
+      <button onClick={props.handleDeleteOption}>remove</button>
     </div>
   );
 }
