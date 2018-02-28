5f198b99fcb6f6ca5af2d9e60574cc01641e5042
Daniel Kreling <dbkreling@gmail.com>
2018-02-28 17:40:32 -0300

    app.js: introduce ES6 import and export
    In the efforts to redesign the application with enhanced technology,
    we explore the import and export features of ES6 syntax, which allow
    us to consume data and structures from other files of the project.
    
    This commit is only using Named Exports.
    
    Ref.: Lecture 51
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+import { square, add } from './utils.js';
+import { isAdult, canDrink } from './person.js';
+
 console.log('app.js is running');
+console.log(square(4));
+console.log(add(3, 2));
+
+console.log('17 is adult: ', isAdult(17));
+console.log('18 is adult: ', isAdult(18));
+console.log('10 can drink: ', canDrink(10));
+console.log('21 can drink: ', canDrink(21));
