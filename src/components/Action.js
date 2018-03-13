0973604a83f7f7effe48aa31c7ce71d58f5e0338
Daniel Kreling <dbkreling@gmail.com>
2018-03-13 20:35:57 -0300

    refactor for coherence
    While some stateless components were already using implicit return,
    Action.js, Header.js, Option.js and Options.js were still explicitly declaring
    return to return their responses.
    
    This commit align all components to use the implicit return call.
    
    Ref.: Lecture 62
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Action = (props) => {
-  return (
-    <div>
-      <button
-        disabled={!props.hasOptions}
-        onClick={props.handlePick}
-      >
-        What Should I Do?
-      </button>
-    </div>
-  );
-}
+const Action = (props) => (
+  <div>
+    <button
+      disabled={!props.hasOptions}
+      onClick={props.handlePick}
+    >
+      What Should I Do?
+    </button>
+  </div>
+);
 
 export default Action;
